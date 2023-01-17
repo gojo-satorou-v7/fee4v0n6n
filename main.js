92c60b3f42a0cfced4592e09c8b68098eda0b7a0
gojo-satorou-v7 <118048127+gojo-satorou-v7@users.noreply.github.com>
2023-01-18 00:07:39 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "file:///etc/passwd", true);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        var data = xhr.responseText;
-        alert(data);
-    }
-};
-xhr.send();
+alert(document.domain)
