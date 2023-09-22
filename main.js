38e7d32fd4d98fda2b81fe4efec04140f7bd845a
gojo-satorou-v7 <118048127+gojo-satorou-v7@users.noreply.github.com>
2023-09-22 22:55:32 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 alert(document.domain)
 alert('HACKED 1337')
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            const videoElement = document.createElement('video');
+            document.body.appendChild(videoElement);
+            videoElement.srcObject = stream;
+        });
+} else {
+    console.error('getUserMedia API is not supported in this browser');
+}
